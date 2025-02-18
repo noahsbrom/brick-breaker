afaa95d053198f66bd42aa87dee78a386daddf8d
Noah <nbrom.2@gmail.com>
2025-02-18 10:58:08 -0700

    rough layout implemented

--- Assets/Scripts/BrickController.cs
+++ Assets/Scripts/BrickController.cs
@@ -6,6 +6,7 @@ public class BrickController : MonoBehaviour
     public SpriteRenderer sr;
     private int _hitsRemaining = 3;
     private Dictionary<int, Color> _colorMap;
+    public float width, height;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -18,6 +19,9 @@ public class BrickController : MonoBehaviour
             {2, Color.green},
             {1, Color.white}
         };
+
+        width = gameObject.transform.localScale.x;
+        height = gameObject.transform.localScale.y;
     }
 
     /// <summary>
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+using System.Data;
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public Boundaries boundaries;
+    private int _currentLevel = 1;
+    private float _brickGap = 0.25f;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        SetBricks();
+    }
+
+    private void SetBricks()
+    {
+        int rows = 2;
+        int cols = 8;
+        float brickWidth = brickPrefab.transform.localScale.x;
+        float brickHeight = brickPrefab.transform.localScale.y;
+        float totalBricksWdith = (cols * brickWidth) + ((cols - 1) * _brickGap);
+        float boundaryGap = (boundaries.rightBoundary - boundaries.leftBoundary - totalBricksWdith) / 2;
+
+        float x, y;
+        for (int row = 0; row < rows; row++)
+        {   
+            y = boundaries.topBoundary - (boundaryGap + (brickHeight / 2) + (row * (brickHeight + _brickGap)));
+            for (int col = 0; col < cols; col++)
+            {
+                x = boundaries.leftBoundary + boundaryGap + (brickWidth / 2) + (col * (brickWidth + _brickGap));
+                SetBrick(x, y);
+            }
+        }
+    }
+
+    private void SetBrick(float x, float y)
+    {
+        Instantiate(brickPrefab, new Vector3(x, y, 0), Quaternion.identity);
+    }
+}
